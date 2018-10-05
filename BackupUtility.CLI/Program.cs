86680a5616a814bac368d98b147799333f6f78e3
konh <konh@yandex.ru>
2018-10-06 01:47:44 +0700

    Test runner added;

--- BackupUtility.CLI/Program.cs
+++ BackupUtility.CLI/Program.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using BackupUtility.Core.BackupManager;
+using BackupUtility.Core.FileManager;
 
 namespace BackupUtility.CLI {
 	class Program {
+		static IFileManager   _localFs = new LocalFileManager();
+		static IBackupManager _manager = new BackupManager(_localFs, _localFs);
+
 		static void Main(string[] args) {
+			EntryPont().Wait();
 		}
+
+		static async Task EntryPont() {
+			Console.WriteLine("Starting...");
+			var sources = new string[] { _localFs.CombinePath("root", "child") };
+			await _manager.Dump(sources, "backup");
+			Console.WriteLine("Backup done.");
+			Console.ReadKey();
+		}  
 	}
 }
